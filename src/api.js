d861f3cde4ab7600e8938f4cc00e8b033721e1e5
Jean Osco <willjean29@gmail.com>
2022-03-13 22:16:07 -0500

    config - add url api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://bsale-api-v1.herokuapp.com",
 });
 
 export const getProducts = async (page = "1", category = "", search = "") => {
